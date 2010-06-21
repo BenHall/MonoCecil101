7045023f00fff3f0c24b10fc7bec589ac8f5451e
BenHall <ben@benhall.me.uk>
2010-06-21 23:20:39 +0100

    Cleaned output

--- src/Prototype/Prototype/Program.cs
+++ src/Prototype/Prototype/Program.cs
@@ -41,11 +41,16 @@ namespace Prototype
                 }
             }
 
+            Console.WriteLine();
+            Console.WriteLine();
+
             //Want to pull this from assembly references... 
             ModuleDefinition actualAssembly = ModuleDefinition.ReadModule(@"D:\SourceControl\MonoCecil101\example\UnitTesting1\UnitTesting1.Tests\bin\Debug\UnitTesting1.dll");
 
             foreach (var type in actualAssembly.Types)
             {
+                Console.WriteLine("Processing... " + type.Name);
+
                 foreach (var method in type.Methods)
                 {
                     IEnumerable<InstructionCall> instructionCalls = instructionsToInspect.Where(x => x.Assembly == type.Scope.Name && x.Method == method.Name);
